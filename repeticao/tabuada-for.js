0092b2ffd65ab04ff1d67dc2d16d2333ad6de13d
Beez <dev.beatreis@gmail.com>
2026-07-26 21:04:26 -0300

    Implement multiplication table for a given number

--- repeticao/tabuada-for.js
+++ repeticao/tabuada-for.js
@@ -1,1 +1,8 @@
+//tabuada
+let numero = 8
+
+console.log ("------ Tabuada do " + numero + " ------ ")
+for (let contador = 0; contador <= 10; contador++){
+  console.log(`${numero} x ${contador} = ${contador * numero}`)
+}
 
