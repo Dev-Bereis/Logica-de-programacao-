aeee575b572814931f9ca5aa750d00ef7b9f8632
Dev-Bereis <dev.beatreis@gmail.com>
2026-07-27 12:14:57 -0300

    Adicionando novos Aprendizados

---
+++ vetor/vetor.js
@@ -0,0 +1,6 @@
+let nomesPokemon = ["Pikachu", "Chamander", "Bulbassar"] //vetor
+let timePokemon = [
+    ["Pikachu", "M", "level 1"],
+    ["Charmander", "F", "level 4"] 
+]
+console.log(nomesPokemon.length)
