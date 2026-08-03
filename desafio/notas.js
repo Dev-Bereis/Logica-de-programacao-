b10209974458f08e426371fecee46db40b8f2d1f
Dev-Bereis <dev.beatreis@gmail.com>
2026-08-03 10:58:13 -0300

    Novos aprendizados

--- desafio/notas.js
+++ desafio/notas.js
@@ -1,18 +1,18 @@
 // desafio de notas
 let aluno = "Breno"
-let notaFinal = 10
+let notaFinal = 7.69
 let situacao = " "
 
 if(notaFinal < 5){
-    situacao = " reprovado."
+    situacao = "reprovado."
 
 }else if(notaFinal <= 6.9){
-    situacao = " recuperação."
+    situacao = "recuperação."
 
 }else if( notaFinal <= 8.9){
-    situacao = " Aaprovado"
+    situacao = "aprovado"
 }else{
-    situacao = " aprovado com honras"
+    situacao = "aprovado com honras"
 }
 
-console.log("O Aluno " + aluno + " foi" + situacao + " pois a sua nota final foi " + notaFinal + ".")
+console.log(`O Aluno ${aluno} foi ${situacao} pois a sua nota final foi ${notaFinal}.`)
---
+++ funcoes/function.js
@@ -0,0 +1,14 @@
+// primeira função
+torrar()
+injetarPao()
+
+
+function torrar(){
+    console.log("Torrando pão")
+    injetarPao()
+}
+
+function injetarPao(){
+    console.log("Preparando para injetar o pão")
+    console.log("finalizado")
+}
